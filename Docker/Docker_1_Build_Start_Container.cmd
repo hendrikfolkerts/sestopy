b4c0e4ef8425c60fad775568d44e2e28cc9a0593
hendrikfolkerts <47664627+hendrikfolkerts@users.noreply.github.com>
2021-03-09 19:43:10 +0100

    Delete Docker_1_Build_Start_Container.cmd

--- Docker/Docker_1_Build_Start_Container.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-REM create folder to share between host and container (one directory above the folder containing this script)
-mkdir .\..\files_for_SESToPy
-REM run a container from the image -> the program is in the /app folder where it is started from, a folder for files for the program is linked as well
-docker run --name=sestopycontainer -it -v /tmp/.X11-unix:/tmp/.X11-unix -v $(pwd)/../files_for_SESToPy:/files_from_host -e DISPLAY=$DISPLAY -u qtuser hf/sestopy python3 /app/main.py
-
-pause
