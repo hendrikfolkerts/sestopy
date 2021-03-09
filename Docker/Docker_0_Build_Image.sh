7181139628d308b6f8ce5d47d6d3aa1516ea65ac
hendrikfolkerts <47664627+hendrikfolkerts@users.noreply.github.com>
2021-03-09 19:43:03 +0100

    Delete Docker_0_Build_Image.sh

--- Docker/Docker_0_Build_Image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#go to the directory of this script
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-#cd $DIR
-cd "$(dirname "$0")"
-#build the image
-docker build -t hf/sestopy .
